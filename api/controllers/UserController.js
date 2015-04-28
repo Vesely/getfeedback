857d558808516f107a6b4e2176d6a1ef538a3eae
David Veselý <davidvesely.cz@gmail.com>
2015-04-28 10:20:02 +0200

    Style update

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,5 +7,30 @@
 
 module.exports = {
 	
+	/**
+	 * `UserController.findOrCreateUser()`
+	 *
+	 * Add Design and upload file to the server's disk.
+	 */
+	findOrCreateUser: function (req, res) {
+
+		//Variables
+		var email = req.param("email");
+		
+		//Find user by email, if not found, create him.
+		User.findOrCreate({email: email}, {email: email}).exec(function(err, usr){
+			if (err) {
+				res.send(500, { error: "DB Error" });
+			} else {
+				if (usr) {
+					res.json(usr);
+				} else {
+					sails.log.info('Nenašel jsem uživatele');
+					res.redirect('/');
+				}
+				
+			}
+		});
+	},
 };
 
