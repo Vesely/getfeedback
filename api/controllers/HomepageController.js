5b2fa601acca0e1e29e8b627bc4f1a8845a0d3cf
David Veselý <davidvesely.cz@gmail.com>
2015-04-28 11:19:24 +0200

    Clear homepage controller

--- api/controllers/HomepageController.js
+++ api/controllers/HomepageController.js
@@ -7,23 +7,5 @@
 
 module.exports = {
 	
-
-
-  /**
-   * `HomepageController.index()`
-   */
-  index: function (req, res) {
-    console.log('test');
-    sails.log.info('Hello. It is works!!');
-    return res.view('homepage', {
-    	test: 'I am awesome! '
-    });
-    
-
-    // return res.send("Hi there!");
-    // return res.json({
-    //   test: 'hi() is not implemented yet!'
-    // });
-  }
 };
 
